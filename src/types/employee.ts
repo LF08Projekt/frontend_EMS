3ea5723947f1b7197dd8c081d2770f3d6718971f
Armin Ribic <arminribic@web.de>
2026-01-14 13:26:25 +0100

    Alle Endpunkte implementiert

---
+++ src/types/employee.ts
@@ -0,0 +1,28 @@
+//"Richtiges Format wie im BE"
+export interface Qualification {
+    id: number;
+    skill: string;
+}
+
+//"Richtiges Format wie im BE"
+export interface Employee {
+    id: number;
+    firstName: string;
+    lastName: string;
+    street: string;
+    postcode: string;
+    city: string;
+    phone: string;
+    skillSet: Qualification[];
+}
+
+// Für POST(create) /employees weil hier keine ID übergeben wird
+export interface CreateEmployeeDto {
+    firstName: string;
+    lastName: string;
+    street: string;
+    postcode: string;
+    city: string;
+    phone: string;
+    skillSet: Qualification[]; // kann leer sein, [] erlaubt
+}
